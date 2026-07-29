d8b64c5b83f6704375c0af4026784dd35b411acd
nyrjan228 <mr.nyrjan@gmail.com>
2026-07-29 11:25:11 +0500

    677

--- bf_parser.py
+++ bf_parser.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent
 # Конфигурация
 # ---------------------------------------------------------------------------
 
-START = 645
+START = 677
 
 TIMEOUT = aiohttp.ClientTimeout(total=45)
-CONCURRENCY_LIMIT = 5
+CONCURRENCY_LIMIT = 2
 RETRY_BASE_DELAY = 3
 
 HIERARCHY_BATCH_SIZE = 5
@@ -639,7 +639,6 @@ async def run() -> None:
     print(f"Загружено {len(hierarchy_rows)} Industry URL, разбито на {len(batches)} батчей по {HIERARCHY_BATCH_SIZE}.")
 
     proxies = [
-        "37.187.132.179:56513:236843:236843",
         "37.187.132.178:49587:236843:236843"
     ]
     
