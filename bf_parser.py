c56a8eefda0d207040532c889fa57aa4d4a2a005
nyrjan228 <mr.nyrjan@gmail.com>
2026-07-31 19:50:26 +0500

    961

--- bf_parser.py
+++ bf_parser.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent
 # Конфигурация
 # ---------------------------------------------------------------------------
 
-START = 956
+START = 961
 
 TIMEOUT = aiohttp.ClientTimeout(total=45)
 CONCURRENCY_LIMIT = 6
